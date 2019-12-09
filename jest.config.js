17337b75d7ef41ddb2778f106a082555b06ba0f5
Mike <mibberson@3merge.ca>
2019-12-09 17:55:11 -0500

    fix: coverage directory for coveralls

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,6 @@
 module.exports = {
   verbose: true,
-  cacheDirectory: '.jest-cache',
-  coverageDirectory: '.jest-coverage',
-  coverageReporters: ['json-summary', 'text', 'lcov'],
-  testPathIgnorePatterns: [
-    '<rootDir>/node_modules/',
-  ],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
   transform: {
     '^.+\\.jsx?$': 'babel-jest',
   },
